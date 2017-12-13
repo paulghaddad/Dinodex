71b4c3a525dba91d02d4ea4bb756680a8c9aadbd
Paul Haddad <paulh16@gmail.com>
2017-12-13 07:03:11 -0500

    Normalize the input data

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import csv
 
-dinosaurs = [dinosaurs for dinosaurs in csv.DictReader(open("dinodex.csv"))]
+dinosaurs = []
 
-print(dinosaurs[0])
+def normalize_data(row):
+    for attribute in row:
+        if row[attribute] == '':
+            row[attribute] = None
+    return row
+
+with open("dinodex.csv") as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        dinosaur = normalize_data(row)
+        dinosaurs.append(dinosaur)
+
+print(dinosaurs)
