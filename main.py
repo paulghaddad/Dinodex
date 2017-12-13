f84cdfa28070748c42c3e4a562641029917024b9
Paul Haddad <paulh16@gmail.com>
2017-12-13 07:17:45 -0500

    Output as a CSV

--- main.py
+++ main.py
@@ -14,4 +14,11 @@ with open("dinodex.csv") as csvfile:
         dinosaur = normalize_data(row)
         dinosaurs.append(dinosaur)
 
-print(dinosaurs)
+fields = ['NAME', 'PERIOD', 'CONTINENT', 'DIET', 'WEIGHT_IN_LBS', 'WALKING',
+          'DESCRIPTION']
+
+dict_writer = csv.DictWriter(open("dinosaur_output.csv", "w"), fieldnames=fields)
+
+dict_writer.writeheader()
+dict_writer.writerows(dinosaurs[1:])
+del dict_writer
