9da0c404faad7c67b1731a7de69faed6ba7c6816
Paul Haddad <paulh16@gmail.com>
2017-12-14 07:38:49 -0500

    Output dinosaur data to a json file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import csv
+import json
 from openpyxl import Workbook
 from prettytable import PrettyTable
 
@@ -20,7 +21,7 @@ fields = ['NAME', 'PERIOD', 'CONTINENT', 'DIET', 'WEIGHT_IN_LBS', 'WALKING',
           'DESCRIPTION']
 
 # Output to a CSV
-dict_writer = csv.DictWriter(open("dinosaur_output.csv", "w"), fieldnames=fields)
+dict_writer = csv.DictWriter(open("dinosaur_data.csv", "w"), fieldnames=fields)
 
 dict_writer.writeheader()
 dict_writer.writerows(dinosaurs[1:])
@@ -36,7 +37,12 @@ ws.append(fields)
 for row in dinosaurs:
     ws.append(list(row.values()))
 
-wb.save("dinosaur_output.xlsx")
+wb.save("dinosaur_data.xlsx")
+
+# Output to a JSON file
+json_outfile = open("dinosaur_data.json", "w")
+json.dump(dinosaurs, json_outfile)
+json_outfile.close()
 
 # Ouput to the command line
 table = PrettyTable(fields)
