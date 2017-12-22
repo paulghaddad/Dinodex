898109700a33e509a6b1e5f440512ab75f6faa09
Paul Haddad <paulh16@gmail.com>
2017-12-22 07:24:47 -0500

    Stringify any attributes for excel export

--- excel_export.py
+++ excel_export.py
@@ -1,5 +1,14 @@
 from openpyxl import Workbook
 
+def stringify_values(values):
+    stringified_values = []
+    for value in values:
+        if isinstance(value, list):
+            stringified_values.append(', '.join(value))
+        else:
+            stringified_values.append(value)
+    return stringified_values
+
 def export_excel(fields, data):
     wb = Workbook()
     ws = wb.active
@@ -8,6 +17,7 @@ def export_excel(fields, data):
     ws.append(fields)
 
     for row in data:
-        ws.append(list(vars(row).values()))
+        cells = stringify_values(vars(row).values())
+        ws.append(cells)
 
     wb.save("dinosaur_data.xlsx")
